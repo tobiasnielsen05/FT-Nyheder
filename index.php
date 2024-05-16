c7f89632b82b735007e2a12dada8ac5c7f949ee3
Tobias <ton001@edu.zealand.dk>
2024-05-16 08:50:15 +0200

    Tilføjet seneste nyt

--- index.php
+++ index.php
@@ -14,31 +14,29 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-<body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<body class="bg-secondarybg">
+<nav class="navbar navbar-expand-lg bg-primary mb-5">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">FOLKETIDENDE</a>
+        <a class="navbar-brand text-white fw-bold" href="index.php">FOLKETIDENDE</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link text-white" href="#">Features</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    <a class="nav-link text-white" href="#">Pricing</a>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
+
+<div class="text-center bg-latestnews">
+    <h4>Seneste Nyt</h4>
+</div>
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
