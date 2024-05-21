84c4e8c851d97806b0ccdf17bddf5cf912ee274c
Tobias <ton001@edu.zealand.dk>
2024-05-21 11:54:06 +0200

    Tilføjet plads kommentarer + tilføjet indhold

---
+++ debat.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+	<meta charset="utf-8">
+	
+	<title>Debatten | Folketidende</title>
+	
+	<meta name="robots" content="All">
+	<meta name="author" content="Udgiver">
+	<meta name="copyright" content="Information om copyright">
+	
+	<link href="css/styles.css" rel="stylesheet" type="text/css">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="bg-secondarybg">
+<nav class="navbar navbar-expand-lg bg-primary mb-3">
+    <div class="container-fluid">
+        <a class="navbar-brand text-white fw-bold" href="index.php">FOLKETIDENDE</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Pricing</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="row">
+    <div class="bg-secondary text-white d-flex justify-content-center">
+        <h1 class="mt-1 text-primary">Debat</h1>
+    </div>
+
+    <div class="bg-background">
+        <div class="d-flex justify-content-center">
+            <h1 class="fs-2 mt-2 m-2 text-primary mb-3">Hvordan skal Lolland kommune sørge
+                for at folkeskolerne ikke dumper?</h1>
+        </div>
+        <div class="d-flex justify-content-center bg-secondary">
+            <h1 class="mt-2">*Billede*</h1>
+        </div>
+        <div>
+            <p class="m-2 mt-2 mb-3 fs-4">
+                Som i alle ved er lollands <br>
+                folkeskoler i seriøse problemer når <br>
+                det gælder deres  beståelsesprocent.
+                <br>
+                <br>
+                Hvis i har læst om dette problem har i
+                sikkert også nogle holdninger, her kan
+                i frit skrive dem ned.
+                <br><br>
+                Hold den gode tone og forbliv
+                respektfuld overfor andre.
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="row mt-4">
+    <div class="bg-secondary text-white d-flex justify-content-center">
+        <h1 class="mt-1 fw-bold text-primary">Kommentarer</h1>
+    </div>
+</div>
+
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
