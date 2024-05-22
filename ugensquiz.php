fa65a7b963e367142a687f072b3b992d05f42dc9
Tobias <ton001@edu.zealand.dk>
2024-05-22 10:34:38 +0200

    Lavet ugens quiz side + tilføjet det visuelle på siden

---
+++ ugensquiz.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+	<meta charset="utf-8">
+	
+	<title>Ugens Quiz | Folketidende</title>
+	
+	<meta name="robots" content="All">
+	<meta name="author" content="Udgiver">
+	<meta name="copyright" content="Information om copyright">
+	
+	<link href="css/styles.css" rel="stylesheet" type="text/css">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="bg-secondarybg w-100">
+<nav class="navbar navbar-expand-lg bg-primary mb-3">
+    <div class="container-fluid">
+        <a class="navbar-brand text-white fw-bold" href="index.php">FOLKETIDENDE</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Pricing</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+    <div class="row">
+        <div class="bg-latestnews text-background d-flex justify-content-center">
+            <h1 class="mt-1">Uge 18 - Hvad er der sket?</h1>
+        </div>
+    </div>
+
+    <div class="d-flex justify-content-center bg-background mt-3">
+        <h1 class="mt-1">*Billede*</h1>
+    </div>
+
+    <div class="row mt-5">
+        <div class="bg-secondary text-primary d-flex justify-content-center">
+            <h1 class="mt-1">Spørgsmål 1</h1>
+        </div>
+
+        <div class="d-flex justify-content-center mb-3 mt-3">
+            <button class="w-100 mt-4 bg-secondary text-primary fs-1 rounded rounded-4 border-0">Svarmulighed 1</button>
+        </div>
+
+        <div class="d-flex justify-content-center mb-3">
+            <button class="w-100 mt-3 bg-secondary text-primary fs-1 rounded rounded-4 border-0">Svarmulighed 2</button>
+        </div>
+
+        <div class="d-flex justify-content-center mb-3">
+            <button class="w-100 mt-3 mb-3 bg-secondary text-primary fs-1 rounded rounded-4 border-0">Svarmulighed 3</button>
+        </div>
+
+</div>
+
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
