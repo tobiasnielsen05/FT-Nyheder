4162fdcf9a62a9c71a93c8af135998f8e7363930
Tobias <ton001@edu.zealand.dk>
2024-05-27 09:36:22 +0200

    Man kan nu indsætte en kommentar

--- debat.php
+++ debat.php
@@ -1,27 +1,39 @@
 <?php
 require "settings/init.php";
+
+if (!empty($_POST["data"])) {
+    $data = $_POST["data"];
+
+    $sql = "INSERT INTO kommentarer (komNavn, komTekst) VALUES (:komNavn, :komTekst)";
+    $bind = [":komNavn" => $data["komNavn"], ":komTekst" => $data["komTekst"]];
+
+    $db->sql($sql, $bind, false);
+
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="da">
 <head>
-	<meta charset="utf-8">
-	
-	<title>Debatten | Folketidende</title>
-	
-	<meta name="robots" content="All">
-	<meta name="author" content="Udgiver">
-	<meta name="copyright" content="Information om copyright">
-	
-	<link href="css/styles.css" rel="stylesheet" type="text/css">
-	
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+
+    <title>Debatten | Folketidende</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body class="bg-secondarybg w-100">
 <nav class="navbar navbar-expand-lg bg-primary mb-3">
     <div class="container-fluid">
         <a class="navbar-brand text-white fw-bold" href="index.php">FOLKETIDENDE</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
@@ -54,7 +66,7 @@ require "settings/init.php";
     </div>
 
     <div class="d-flex justify-content-center">
-        <p class="mb-3 mt-4 fs-4 text-primary">
+        <p class="mb-4 mt-4 fs-4 text-primary">
             Som i alle ved er lollands folkeskoler i seriøse problemer når det gælder deres beståelsesprocent.
             <br><br>
             Hvis i har læst dette problem har i sikkert også nogle holdninger. <br>
@@ -65,19 +77,22 @@ require "settings/init.php";
     </div>
 
 
-    <form action="">
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label fs-4 fw-bold text-primary">Her kan du skrive en kommentar</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Navn">
-        </div>
-        <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label fs-5 fw-bold text-primary">Kommentar</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
-    </form>
-
-    <div class="d-flex justify-content-center mb-3">
-        <button class="w-100 mt-2 mb-4 bg-orange text-white fw-bold fs-1 rounded rounded-4 border-0">Send Kommentar</button>
+    <div class="container mb-5">
+        <form action="insert.php" method="post">
+            <div class="row g-3">
+                <div class="col-12 col-md-6">
+                    <label for="komNavn" class="form-label">Navn</label>
+                    <input type="text" class="form-control" id="komNavn" name="data[komNavn]" placeholder="Dit navn" value="">
+                </div>
+                <div class="col-12">
+                    <label for="komTekst" class="form-label">Kommentar</label>
+                    <textarea class="form-control" name="data[komTekst]" id="komTekst" placeholder="Din kommentar"></textarea>
+                </div>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-orange text-white fw-bold w-100">Indsend Kommentar</button>
+                </div>
+            </div>
+        </form>
     </div>
 
     <div class="row bg-background mb-5">
@@ -91,7 +106,8 @@ require "settings/init.php";
 </div>
 
 <footer class="bg-primary d-flex justify-content-center">
-    <a class="d-flex justify-content-center" href="index.php"><img class="w-25" src="images/pil.svg" alt="pil der går tilbage til forsiden"></a>
+    <a class="d-flex justify-content-center" href="index.php"><img class="w-25" src="images/pil.svg"
+                                                                   alt="pil der går tilbage til forsiden"></a>
 </footer>
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
